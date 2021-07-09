e7d4e493ac5cb94ac9d8f7ad96c7329a07dbbffc
Abhishek Kumar <abhi967792@gmail.com>
2021-07-09 21:06:16 +0530

    Sidebar work done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-
+import './App.css'
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <div className="container">
+        <Sidebar/>
+        <div className="other">
+          OtherPages
+        </div>
+      </div>
       
     </div>
   );
